3bbbc1b5cc3ff228ba0259bb5ffef810c9be0651
sailalith <lalithvoop@hotmail.com>
2024-06-12 23:07:32 +0530

    adding env in shell file: added parameter flag e

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 role=$1
 
-ansible-playbook -i $role-$env.vsldo.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 ansible.yml -e role=$role env=$env
+ansible-playbook -i $role-$env.vsldo.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 ansible.yml -e role=$role -e env=$env
