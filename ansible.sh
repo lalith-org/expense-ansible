c0739acb0d0b49d4a66c63c97d519f3c35f40936
sailalith <lalithvoop@hotmail.com>
2024-06-23 14:48:21 +0530

    updated ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -2,5 +2,8 @@ role=$1
 
 #ansible-playbook -i $role-$env.vsldo.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 ansible.yml -e role=$role -e env=$env
 
-ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$role
-ansible-playbook -i $component-$env.vsldo.online, -e env=$env -e role_name=$role ansible.yml -e '@~/secrets.json' -e '@~/app.json'
+#ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role=$role
+#ansible-playbook -i $component-$env.vsldo.online, -e env=$env -e role_name=$role ansible.yml -e '@~/secrets.json' -e '@~/app.json'
+
+ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role=$role
+ansible-playbook -i $component-$env.vsldo.online, -e env=$env -e role=$role ansible.yml -e '@~/secrets.json' -e '@~/app.json'
