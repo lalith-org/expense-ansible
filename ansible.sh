22e86a66afa6a7c832fceebc7670945885552e4f
sailalith <lalithvoop@hotmail.com>
2024-07-29 19:17:45 +0530

    fixing changes

--- ansible.sh
+++ ansible.sh
@@ -6,5 +6,7 @@ role=$1
 #ansible-playbook -i $component-$env.vsldo.online, -e env=$env -e role_name=$role ansible.yml -e '@~/secrets.json' -e '@~/app.json'
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role=$role
-ansible-playbook -i $role-$env.vsldo.online, -e env=$env -e role=$role ansible.yml -e '@~/secrets.json' -e '@~/app.json'
-rm -f ~/secrets.json ~/app.json
+#ansible-playbook -i $role-$env.vsldo.online, -e env=$env -e role=$role ansible.yml -e '@~/secrets.json' -e '@~/app.json'
+aws ec2 describe-instances --filters Name=tag:Name,Values=$role-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
+ansible-playbook -i inv -e env=$env -e role_name=$role expense.yml -e '@~/secrets.json'
+rm -f ~/*.json
