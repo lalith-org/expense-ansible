1400be09f9421838b62187511aa2b4ca5711cc70
sailalith <lalithvoop@hotmail.com>
2024-07-31 14:31:02 +0530

    added hostname

--- ansible.sh
+++ ansible.sh
@@ -4,7 +4,7 @@ role=$1
 
 #ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role=$role
 #ansible-playbook -i $component-$env.vsldo.online, -e env=$env -e role_name=$role ansible.yml -e '@~/secrets.json' -e '@~/app.json'
-
+echo hostname
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role=$role
 #ansible-playbook -i $role-$env.vsldo.online, -e env=$env -e role=$role ansible.yml -e '@~/secrets.json' -e '@~/app.json'
 aws ec2 describe-instances --filters Name=tag:Name,Values=$role-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
