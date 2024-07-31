702075862bf0f971c407b4ccf0f0fb02a58fe087
sailalith <lalithvoop@hotmail.com>
2024-07-31 14:36:21 +0530

    whoami

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 role=$1
 
 hostname
-uname
+whoami
 exit
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role=$role
 aws ec2 describe-instances --filters Name=tag:Name,Values=$role-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
