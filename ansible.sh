eb8eaeb3bde00c487e3d92d8e9bf98839f815067
sailalith <lalithvoop@hotmail.com>
2024-07-31 14:39:00 +0530

    removed unwanted data

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,6 @@
 role=$1
 
+rm -f ~/*.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role=$role
 aws ec2 describe-instances --filters Name=tag:Name,Values=$role-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 ansible-playbook -i inv -e env=$env -e role=$role ansible.yml -e '@~/secrets.json'
